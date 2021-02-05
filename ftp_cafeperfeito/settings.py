023c0c17cbe404f255bfcccdc24508c53fec7d7a
thiagomacedo <tl.macedo@hotmail.com>
2021-02-05 14:42:55 -0400

    auto commit

--- ftp_cafeperfeito/settings.py
+++ ftp_cafeperfeito/settings.py
@@ -13,8 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -88,28 +88,28 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # },
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-
-        # 'NAME': 'cafeperfeito',
-        # 'USER': 'cafeperfeito',
-        # 'PASSWORD': 'Tlm487901',
-        # 'HOST': 'mysql.cafeperfeito.com.br',
-
-        'NAME': 'cafeperfeito_v2.03',
-        'USER': 'root',
-        'PASSWORD': 'Tlm487901',
-        'HOST': '127.0.0.1',
-
-        'PORT': '3306',
-        # 'TEST': {
-        #     'NAME': 'testecafeperfeito',
-        # },
-        'OPTIONS': {
-            # 'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
-    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #
+    #     # 'NAME': 'cafeperfeito',
+    #     # 'USER': 'cafeperfeito',
+    #     # 'PASSWORD': 'Tlm487901',
+    #     # 'HOST': 'mysql.cafeperfeito.com.br',
+    #
+    #     'NAME': 'cafeperfeito_v2.03',
+    #     'USER': 'root',
+    #     'PASSWORD': 'Tlm487901',
+    #     'HOST': '127.0.0.1',
+    #
+    #     'PORT': '3306',
+    #     # 'TEST': {
+    #     #     'NAME': 'testecafeperfeito',
+    #     # },
+    #     'OPTIONS': {
+    #         # 'charset': 'utf8mb4',
+    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+    #     }
+    # },
 }
 
 # Password validation
@@ -151,8 +151,6 @@ USE_TZ = True
 # ]
 
 STATIC_URL = '/static/'
-
-MEDIA_URL = '/media/'
 
 STATIC_ROOT = '/home/cafeperfeito/www/static'
 
