a7f908f5ef19704a157c8ae24a3c53de9f7d3b8c
thiagomacedo <tl.macedo@hotmail.com>
2021-02-08 11:03:25 -0400

    auto commit

--- ftp_cafeperfeito/views.py
+++ ftp_cafeperfeito/views.py
@@ -2,4 +2,5 @@ from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    # template_name = 'index.html'
+    template_name = 'manutenção.html'
