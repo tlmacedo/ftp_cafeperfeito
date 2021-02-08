368017914d1f14cdf843ed2182cb8af93a290750
thiagomacedo <tl.macedo@hotmail.com>
2021-02-08 16:57:49 -0400

    auto commit

--- sidtm/urls.py
+++ sidtm/urls.py
@@ -13,22 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from .views import ManutencaoView
+from sidtm.views import IndexView
 
-# app_name = 'cafeperfeito'
+app_name = 'cafeperfeito'
 
 urlpatterns = [
-    path('', include('cafeperfeito.urls')),
-    path('', ManutencaoView.as_view(), name='manutencao'),
-    # path('', IndexView.as_view(), name='index'),
-    path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
 ]
 
-if settings.DEBUG:
-    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
